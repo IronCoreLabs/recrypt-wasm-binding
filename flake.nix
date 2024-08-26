7c8d4c283b69db493674835524f730a3204a82c4
Murph Murphy <murph@clurictec.com>
2024-08-26 16:34:30 -0600

    Update to fixed micromatch

--- flake.nix
+++ flake.nix
@@ -11,28 +11,6 @@
         pkgs = import nixpkgs { inherit system overlays; };
         rusttoolchain =
           pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        # wasm-bindgen = pkgs.rustPlatform.buildRustPackage rec {
-        #   pname = "wasm-bindgen";
-        #   version = "0.2.93";
-
-        #   nativeBuildInputs = with pkgs; [ pkg-config ];
-
-        #   src = pkgs.fetchCrate {
-        #     inherit pname version;
-        #     hash = "sha256-LatHkqF6y+XtY2S/6KJWOjmkB5NcKKfmrhEWmAACLMM=";
-        #   };
-
-        #   cargoLock.lockFile = ./wasm-bindgen-Cargo.lock;
-        #   cargoLock.outputHashes = {
-        #     "raytracer-0.1.0" = "sha256-k6emdBDunYK4pUxrwJCbm57LzICj+q4bRAJ/XJ0zsg0=";
-        #     "weedle-0.13.0" = "sha256-S/AzZmEPamYt0vT6eM8fxnZmXWXwV1DLxVlLIYemZYc=";
-        #   };
-        #   cargoPatches = [
-        #     ./wasm-bindgen-Cargo.lock
-        #   ];
-
-        #   cargoHash = "";
-        # };
       in
       {
         devShell = pkgs.mkShell {
